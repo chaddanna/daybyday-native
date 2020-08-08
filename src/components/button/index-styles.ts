f4fba3cde80d94686b21e2e6eeee65ccefc3ce41
chaddanna <chad@formulated.io>
2020-08-08 08:08:22 -0600

    fix: Add border-radius

--- src/components/button/index-styles.ts
+++ src/components/button/index-styles.ts
@@ -4,11 +4,12 @@ import { colors, typography } from "../../styles";
 export const styles = StyleSheet.create({
   button: {
     backgroundColor: colors.primary,
+    borderRadius: 8,
     marginTop: 20,
   },
   buttonText: {
     fontSize: typography.text,
     textAlign: "center",
-    paddingVertical: 10,
+    paddingVertical: 18,
   },
 });
