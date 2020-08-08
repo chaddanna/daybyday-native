234caad34be455ab30bfcf265e766cd09a8863b0
chaddanna <chad@formulated.io>
2020-08-08 10:24:29 -0600

    fix: Add strict typing for children

--- src/components/screen-title/index.tsx
+++ src/components/screen-title/index.tsx
@@ -2,6 +2,10 @@ import React, { FC } from "react";
 import { Text } from "react-native";
 import { styles } from "./index-styles";
 
-export const ScreenTitle: FC = ({ children }) => (
+type ScreenTitleProps = {
+  children: string;
+};
+
+export const ScreenTitle: FC<ScreenTitleProps> = ({ children }) => (
   <Text style={styles.title}>{children}</Text>
 );
