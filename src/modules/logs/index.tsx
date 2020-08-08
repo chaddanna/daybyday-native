9dd16e584f57724c24b8db3317d41c4c0d11b921
chaddanna <chad@formulated.io>
2020-08-08 10:25:51 -0600

    feat: Add tasks data to logs

--- src/modules/logs/index.tsx
+++ src/modules/logs/index.tsx
@@ -1,11 +1,32 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useContext } from "react";
+import { ScrollView } from "react-native";
 import { Container } from "../../components/container";
+import { ScreenTitle } from "../../components/screen-title";
+import { Task, TaskContext } from "../../contexts/task";
+import { Log } from "./components/log";
 
 export function Logs() {
+  const { tasks } = useContext(TaskContext);
+  function completed(task: Task) {
+    return task.isComplete === true;
+  }
+  function latestFirst(a: Task, b: Task) {
+    if (a.dateCompleted && b.dateCompleted) {
+      return +new Date(b.dateCompleted) - +new Date(a.dateCompleted);
+    }
+    return +new Date();
+  }
   return (
     <Container>
-      <Text>Logs page</Text>
+      <ScreenTitle>Logs</ScreenTitle>
+      <ScrollView>
+        {tasks
+          .filter(completed)
+          .sort(latestFirst)
+          .map((t) => (
+            <Log key={t.label} log={t}></Log>
+          ))}
+      </ScrollView>
     </Container>
   );
 }
